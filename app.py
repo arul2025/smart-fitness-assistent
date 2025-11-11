da5ecc98cef624b418ddff0e52498eafe99a26fc
arul2025 <aruljothi@jainvidyalaya.in>
2025-11-11 12:37:03 +0530

    Update API key configuration method
    Replaced the API key retrieval from Streamlit Secrets with a hardcoded API key.

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ st.markdown("#### Personalized AI Coaching for Young Athletes")
 
 # Load Gemini API key (stored securely in Streamlit Secrets)
 # Add this in Streamlit Cloud -> Settings -> Secrets -> GCP_API_KEY = "your_api_key_here"
-api_key = st.secrets[GCP_API_KEY]
-genai.configure(api_key=api_key)
+ 
+genai.configure(api_key="AIzaSyDj1wNAWvAB5GcbKa657dUGeXlnIMLGR88")
 
 # ----------------------------
 # SIDEBAR USER INPUTS
